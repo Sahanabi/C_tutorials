b6256fa018753c6d5be053ed4127b65ab7bc3109
Sahana B Ilager <138303397+Sahanabi@users.noreply.github.com>
2025-08-25 09:48:27 +0530

    ternary_greater_no.c

--- ternary_greater_no.c
+++ ternary_greater_no.c
@@ -6,4 +6,19 @@ int main(){
      scanf("%d %d",&a,&b);
 
      (a>b)?printf("a is greater than b"):printf("b is greater than a");
+
+}
+
+
+//WAP using ternary operator to check which number is greater among 3
+#include <stdio.h>
+int main() {
+    int a, b, c, max;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+    printf("Maximum is: %d\n", max);
+    return 0;
 }
